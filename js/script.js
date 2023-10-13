bc41c5239262b7c19d223674a027c557ad484918
angelo friello <angelofriello01@gmail.com>
2023-10-13 16:43:26 +0200

    add select difficoltà

--- js/script.js
+++ js/script.js
@@ -1,33 +1,87 @@
-const gridDOMElement = document.querySelector('.grid')
-console.log(gridDOMElement)
-const playBtnDOMElement = document.querySelector('.play-btn')
-console.log(playBtnDOMElement)
+const selectDOMElement = document.getElementById('select');
+console.log(selectDOMElement);
+const playBtnDOMElement = document.querySelector('.play-btn');
+
+const gridEasyDOMElement = document.querySelector('.grid-easy');
+const gridMidDOMElement = document.querySelector('.grid-mid');
+const gridHardDOMElement = document.querySelector('.grid-hard');
 
 
 playBtnDOMElement.addEventListener('click', function (){
-    gridDOMElement.innerHTML = '';
 
-    for (let i = 1; i <= 100 ;i++){
-        n = i
-        console.log(n)
-        gridDOMElement.innerHTML += `<div class="cell ">${n}</div>`
+    gridEasyDOMElement.innerHTML = '';
+    gridMidDOMElement.innerHTML = '';
+    gridHardDOMElement.innerHTML = '';
+
+    if(selectDOMElement.value == 2){
+
+        for (let i = 1; i <= 100 ;i++){
+            n = i;
+            console.log(n);
+            gridHardDOMElement.innerHTML += `<div class="cell ">${n}</div>`
+        }
+
+        const cellDOMElements = document.querySelectorAll('.cell')
+        console.log(cellDOMElements)
+
+        for (let i = 0; i < cellDOMElements.length; i++){
+
+            const currentCellDOMElement = cellDOMElements[i]
+
+            console.log(currentCellDOMElement)
+
+            currentCellDOMElement.addEventListener('click', function() {
+                currentCellDOMElement.classList.add('bg-azurro')
+            console.log('hai selezionato la casella'+ `${i+1}`)
+            })
+
     }
+    }else if (selectDOMElement.value == 1){
 
-    const cellDOMElements = document.querySelectorAll('.cell')
-    console.log(cellDOMElements)
+        for (let i = 1; i <= 81 ;i++){
+            n = i
+            console.log(n)
+            gridMidDOMElement.innerHTML += `<div class="cell ">${n}</div>`
+        }
 
-    for (let i = 0; i < cellDOMElements.length; i++){
+        const cellDOMElements = document.querySelectorAll('.cell')
+        console.log(cellDOMElements)
 
-        const currentCellDOMElement = cellDOMElements[i]
+        for (let i = 0; i < cellDOMElements.length; i++){
 
-        console.log(currentCellDOMElement)
+            const currentCellDOMElement = cellDOMElements[i]
 
-        currentCellDOMElement.addEventListener('click', function() {
-            currentCellDOMElement.classList.add('bg-azurro')
-        console.log('hai selezionato la casella'+ `${i+1}`)
-        })
+            console.log(currentCellDOMElement)
+
+            currentCellDOMElement.addEventListener('click', function() {
+                currentCellDOMElement.classList.add('bg-azurro')
+            console.log('hai selezionato la casella'+ `${i+1}`)
+            })
 
     }
-        
-}
-)
+    }else if(selectDOMElement.value == 0){
+
+        for (let i = 1; i <= 49 ;i++){
+            n = i
+            console.log(n)
+            gridEasyDOMElement.innerHTML += `<div class="cell ">${n}</div>`
+        }
+
+        const cellDOMElements = document.querySelectorAll('.cell')
+        console.log(cellDOMElements)
+
+        for (let i = 0; i < cellDOMElements.length; i++){
+
+            const currentCellDOMElement = cellDOMElements[i]
+
+            console.log(currentCellDOMElement)
+
+            currentCellDOMElement.addEventListener('click', function() {
+                currentCellDOMElement.classList.add('bg-azurro')
+            console.log('hai selezionato la casella'+ `${i+1}`)
+            })
+
+    }
+    }        
+})
+
