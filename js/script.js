c5144f81616524095895baf8be153a8694733b61
angelo friello <angelofriello01@gmail.com>
2023-10-13 14:46:02 +0200

    add ciclo per bg-green

--- js/script.js
+++ js/script.js
@@ -3,10 +3,31 @@ console.log(gridDOMElement)
 const playBtnDOMElement = document.querySelector('.play-btn')
 console.log(playBtnDOMElement)
 
-for (let i = 1; i <= 100;i++ ){
 
-    n = i
-    console.log(n)
+playBtnDOMElement.addEventListener('click', function (){
+    gridDOMElement.innerHTML = '';
 
-gridDOMElement.innerHTML += `<div class="cell">${n}</div>`
+    for (let i = 1; i <= 100 ;i++){
+        n = i
+        console.log(n)
+        gridDOMElement.innerHTML += `<div class="cell">${n}</div>`
+
+       
+    }
+
+    const cellDOMElement = querySelectorall('.cell')
+
+    for (let i = 0; i < cellDOMElement; i++){
+
+        currentCellDOMElement = cellDOMElement[i]
+
+        cellDOMElement[i].addEventListener('click', function() {
+            currentCellDOMElement.classList.add('bg-.green')
+        })
+
+    }
+        
 }
+)
+    
+ 
